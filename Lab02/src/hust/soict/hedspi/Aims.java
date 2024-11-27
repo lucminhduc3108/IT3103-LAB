e76b1ed24f8ef039e905abd94559639075c17b59
lucminhduc3108 <144041824+lucminhduc3108@users.noreply.github.com>
2024-11-27 21:28:04 +0700

    Add files via upload

--- Lab02/src/hust/soict/hedspi/Aims.java
+++ Lab02/src/hust/soict/hedspi/Aims.java
@@ -1,13 +1,14 @@
+package hust.soict.hedspi.aims;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+import hust.soict.hedspi.aims.cart.Cart;
 import java.util.*;
-/**
- *
- * @author admin
- */
+
 public class Aims {
 
     /**
@@ -18,7 +19,7 @@ public class Aims {
     Cart anOrder = new Cart();
 
     //Create new dvd objects and add them to the cart
-    DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+   /* DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
             "Animation", "Roger Allers", 87, 19.95f);
     anOrder.addDigitalVideoDisc(dvd1);
 
@@ -28,14 +29,23 @@ public class Aims {
 
     DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
             "Animation", 18.99f);
-    anOrder.addDigitalVideoDisc(dvd3);
-
-    //print total cost of the items in the cart
-    System.out.print("Total Cost is: ");
-    System.out.println(anOrder.totalCost());
-    //remove testing
-    anOrder.getFreeItem();
-    anOrder.removeDigitalVideoDisc(dvd3);
+    anOrder.addDigitalVideoDisc(dvd3);*/
+    
+    //Test addDigitalVideoDisc by array
+    DigitalVideoDisc[] dvdList = new DigitalVideoDisc[19];
+    for ( int i = 0; i < 19; i++)
+    {
+        dvdList[i] = new DigitalVideoDisc(Integer.toString(i),
+            "Animation", "Roger Allers", 87, 19.95f);
+    }
+    anOrder.addDigitalVideoDisc(dvdList); 
+    
+    DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+            "Animation", "Roger Allers", 87, 19.95f);
+    DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+            "Science Fiction", "George Lucas", 87, 24.95f);
+    anOrder.addDigitalVideoDisc(dvd1, dvd2); 
+    anOrder.printOrder();
     
     }
     
