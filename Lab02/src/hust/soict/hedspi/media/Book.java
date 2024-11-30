475a4ea1da32ee1aaad6d9371ab5171eeb38c2e2
lucminhduc3108 <144041824+lucminhduc3108@users.noreply.github.com>
2024-11-30 11:36:09 +0700

    Add files via upload

--- Lab02/src/hust/soict/hedspi/media/Book.java
+++ Lab02/src/hust/soict/hedspi/media/Book.java
@@ -9,6 +9,37 @@ package hust.soict.hedspi.aims.media;
  *
  * @author admin
  */
+
+import java.util.*;
 public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private List<String> authors = new ArrayList<String>();
+
+    public Book() {
+    }
+    
+    public void addAuthor (String authorName)
+    {
+        if ( !authors.contains(authorName))
+        {
+            authors.add(authorName);
+            System.out.println("Successfully added");
+        }
+        else System.out.println("Already listed");
+    }
+    
+    public void removeAuthor (String authorName)
+    {
+        if ( authors.contains(authorName))
+        {
+            authors.remove(authorName);
+            System.out.println("Successfully removed");
+        }
+        else System.out.println("Not on the list");
+    }
+    
     
 }
