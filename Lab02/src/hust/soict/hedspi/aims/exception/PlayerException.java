8accf57e888952ccd8d23a8494fd09b339d18a38
lucminhduc3108 <144041824+lucminhduc3108@users.noreply.github.com>
2024-12-20 23:43:30 +0700

    Add files via upload

---
+++ Lab02/src/hust/soict/hedspi/aims/exception/PlayerException.java
@@ -0,0 +1,15 @@
+package hust.soict.dsai.aims.exception;
+
+public class PlayerException extends Exception {
+
+	public PlayerException(String string) {
+		// TODO Auto-generated constructor stub
+		System.out.println(string);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
